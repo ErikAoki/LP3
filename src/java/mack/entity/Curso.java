2074cd02ca8ff6ff8d2168874de729effcb05267
AOKI <AOKI@AOKI-PC>
2017-10-18 17:45:27 -0200

    Curso/Disciplina adicionados, mas não feitos ainda

---
+++ src/java/mack/entity/Curso.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mack.entity;
+
+/**
+ *
+ * @author AOKI
+ */
+public class Curso {
+    private int cod_curso;
+    private String nome_curso;
+
+    public int getCod_curso() {
+        return cod_curso;
+    }
+
+    public void setCod_curso(int cod_curso) {
+        this.cod_curso = cod_curso;
+    }
+
+    public String getNome_curso() {
+        return nome_curso;
+    }
+
+    public void setNome_curso(String nome_curso) {
+        this.nome_curso = nome_curso;
+    }
+}
