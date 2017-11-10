4694a79a4017c8666446fcb931446ffa31148a3f
31609546 <31609546@087841-P33S103.macklabs.br>
2017-11-10 08:12:34 -0200

    Arrumando algumas coisas

--- src/java/mack/controllers/AbstractController.java
+++ src/java/mack/controllers/AbstractController.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mack.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
-/**
- *
- * @author joaquim
- */
 public abstract class AbstractController implements Controller {
     
     public HttpServletRequest request;
--- src/java/mack/controllers/ControllerFactory.java
+++ src/java/mack/controllers/ControllerFactory.java
@@ -1,27 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mack.controllers;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author joaquim
- */
 public class ControllerFactory {
 
     public static final Controller getControllerByClass(Class actionClass) {
         try {
             return (Controller) actionClass.newInstance();
-        } catch (java.lang.InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (ClassCastException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
